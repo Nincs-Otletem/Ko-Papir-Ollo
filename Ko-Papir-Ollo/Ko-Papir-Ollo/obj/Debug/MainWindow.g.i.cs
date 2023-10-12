ed89773545f43e491a61de763c8ca7dc8aeac8ef
HorvathMark <kistapir0@gmail.com>
2023-10-12 09:08:47 +0200

    kepek

--- Ko-Papir-Ollo/Ko-Papir-Ollo/obj/Debug/MainWindow.g.i.cs
+++ Ko-Papir-Ollo/Ko-Papir-Ollo/obj/Debug/MainWindow.g.i.cs
@@ -99,4 +99,33 @@ namespace Ko_Papir_Ollo {
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
+        void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.nevbox = ((System.Windows.Controls.TextBox)(target));
+            return;
+            case 2:
+            
+            #line 13 "..\..\MainWindow.xaml"
+            ((System.Windows.Controls.Button)(target)).Click += new System.Windows.RoutedEventHandler(this.Button_Click);
+            
+            #line default
+            #line hidden
+            return;
+            case 3:
+            this.KPO_pipa = ((System.Windows.Controls.RadioButton)(target));
+            return;
+            case 4:
+            this.KPOGS_pipa = ((System.Windows.Controls.RadioButton)(target));
+            return;
+            case 5:
+            this.hiba = ((System.Windows.Controls.Label)(target));
+            return;
+            }
+            this._contentLoaded = true;
+        }
+    }
+}
+
